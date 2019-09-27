0318cf12af6f2fc875069b9847bea92629a014ed
Lenin Mishra <pylenin1992@gmail.com>
2019-09-27 16:56:19 +0200

    Add newsletter model to Django

--- my_blog/blogs/views.py
+++ my_blog/blogs/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
 from .models import Blogs
+from newsletter.models import Signup
+
 def index(request):
     course_blogs = Blogs.objects.filter(is_course=True)
     latest_blogs = Blogs.objects.order_by('-timestamp')[0:3]
+
+    if request.method == 'POST':
+        email=request.POST['email']
+        new_signup = Signup()
+        new_signup.email=email
+        new_signup.save()
+        print(email)
 
     context = {"object_list":course_blogs,
                "latest_blogs": latest_blogs}
